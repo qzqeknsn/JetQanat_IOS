9032749061d34158694f1268f2b192271aac2c3d
qzqeknsn <abylay342@gmail.com>
2025-12-15 17:09:22 +0500

    Initial Commit

---
+++ JetQanat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JetQanat
+//
+//  Created by Abylay Zholdybay on 15.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
